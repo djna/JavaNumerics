c69d1ab6e44c0fb5ee1830ac7e593057a2aa5833
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-11-15 10:39:58 +0000

    Add log4j

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,18 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final Logger LOGGER = LogManager.getLogger();
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        LOGGER.info( "Hello World!" );
     }
 }
