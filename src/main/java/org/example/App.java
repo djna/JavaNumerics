55cf47f96579ffa694ba617f086693b30c657bea
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-11-16 07:23:41 +0000

    Integer to byte conversion

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,66 +13,33 @@ public class App
     public static void main( String[] args )
     {
         LOGGER.info( "Hello World!" );
-        exploreIncrements();
-        exploreAddition();
-        exploreBigNumbers();
-        exploreSameButDifferent();
+        exploreIntToByte(126, 130);
+        exploreIntToByte(510, 514);
+        exploreIntToByte(-514, -510);
     }
 
-    private static void exploreIncrements() {
-         int bigNumber = Integer.MAX_VALUE;
-         int biggerNumber = bigNumber + 1;
-        if ( biggerNumber > bigNumber){
-            LOGGER.info("biggerNumber is bigger");
-        } else {
-            LOGGER.info("biggerNumber is smaller");
+    // explore converting 32 bit int to 8 bit byte
+    private static void exploreIntToByte(int from, int to) {
+        LOGGER.info("From " + from + " to " + to);
+        for ( int b = from; b < to; b++ ) {
+            String bAsIntValue = String.format("%8d", b);
+            String bAsByteValue = String.format("%8d", (byte)b);
+            String bAsBits = numberAsBinary(b, 8);
+            LOGGER.info("int " + bAsIntValue + " converted to byte " + bAsByteValue + " = " + bAsBits);
         }
     }
 
-    private static void exploreAddition() {
-        double pointOne = 0.1;
-        double pointTwo = 0.2;
-        double pointThree = 0.3;
-        double sumValue = pointOne + pointTwo;
-
-        if ( sumValue == pointThree){
-            LOGGER.info("0.1 + 0.2 == 0.3");
-        } else {
-            LOGGER.warn("0.1 + 0.2 != 0.3");
+    private static String numberAsBinary(int b, int bits){
+        String formatString = "%" + bits + "s";
+        String bAsBits = String.format(formatString,
+                        Integer.toBinaryString(b))
+                .replaceAll(" ","0");
+        if ( bAsBits.length() > bits){
+            bAsBits =  bAsBits.substring(bAsBits.length()-8);
         }
 
+        return bAsBits;
     }
 
-    private static void exploreBigNumbers() {
-        double bigNumber = 9007199254740992.0;
-        double biggerNumber = bigNumber + 1;
 
-        if ( biggerNumber == bigNumber){
-            LOGGER.info("Adding did not change the value");
-        } else {
-            LOGGER.info("Adding did change the value");
-        }
-
-    }
-
-    private static void exploreSameButDifferent() {
-
-        calculateShare(20, 5);
-        calculateShare(0, 5);
-        calculateShare(0, 0);
-
-    }
-
-    private static void calculateShare(double toBeShared, double shareBetween) {
-
-        double oneShare = toBeShared / shareBetween;
-        double anotherShare = toBeShared / shareBetween;
-
-        if ( oneShare == anotherShare){
-            LOGGER.info("Same calculation, same answers");
-        } else {
-            LOGGER.info("Same calculation, different answers!");
-        }
-
-    }
 }
