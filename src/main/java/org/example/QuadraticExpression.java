fd5a93c496ebd39ab58e011a1c209521a6a01b1c
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-11-18 10:09:44 +0000

    BigDecimal using sqrt requires Java > 8

--- src/main/java/org/example/QuadraticExpression.java
+++ src/main/java/org/example/QuadraticExpression.java
@@ -1,32 +1,52 @@
 package org.example;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 public class QuadraticExpression {
-    private double a;
-    private double b;
-    private double c;
-    public QuadraticExpression(double a, double b, double c){
+    private final BigDecimal a;
+    private final BigDecimal b;
+    private final BigDecimal c;
+    private MathContext mathContext;
+    public QuadraticExpression(BigDecimal a, BigDecimal b, BigDecimal c){
         this.a = a;
         this.b = b;
         this.c = c;
+        mathContext = new MathContext(10);
+    }
+    public QuadraticExpression(double a, double b, double c){
+        this.a = new BigDecimal(a);
+        this.b = new BigDecimal(b);
+        this.c = new BigDecimal(c);
+        mathContext = new MathContext(10);
     }
 
-    public double getValueFor(double x){
-        double axSquared = a * x * x;
-        double bx = b * x;
-        double value = axSquared + bx + c;
+    public BigDecimal getValueFor(BigDecimal x){
+        BigDecimal axSquared = a.multiply(x, mathContext).multiply(x, mathContext);
+        BigDecimal bx = b.multiply(x, mathContext);
+        BigDecimal roundedC = c.round(mathContext);
+        BigDecimal value = axSquared.add(bx, mathContext).add(roundedC, mathContext);
 
         return value;
     }
 
-    public double[] solve(double solveFor){
-        double equationC = c - solveFor;
-        double bSquaredMinus4ac = (b * b) - ( 4 * a * equationC );
-        double sqrtBSquaredMinus4ac = Math.sqrt(bSquaredMinus4ac);
+    public BigDecimal getValueFor(double x){
+        return getValueFor(new BigDecimal(x));
+    }
 
-        double plusSolution = ((-b) + sqrtBSquaredMinus4ac) / ( 2 * a);
-        double minusSolution = ((-b) - sqrtBSquaredMinus4ac) / ( 2 * a);
+    public BigDecimal[] solve(BigDecimal solveFor){
+        BigDecimal equationC = c.subtract(solveFor);
+        BigDecimal bSquaredMinus4ac = b.multiply(b).subtract( a.multiply(new BigDecimal(4) ).multiply(equationC) );
+        BigDecimal sqrtBSquaredMinus4ac = bSquaredMinus4ac.sqrt(mathContext);
 
-        return new double[] {plusSolution, minusSolution};
+        BigDecimal plusSolution = b.negate().add(sqrtBSquaredMinus4ac).divide(a.multiply(new BigDecimal(2)));
+        BigDecimal minusSolution = b.negate().subtract(sqrtBSquaredMinus4ac).divide(a.multiply(new BigDecimal(2)));
+
+        return new BigDecimal[] {plusSolution, minusSolution};
+    }
+
+    public BigDecimal[] solve(double solveFor){
+        return solve(new BigDecimal(solveFor));
     }
 
     @Override
--- src/test/java/org/example/QuadraticTest.java
+++ src/test/java/org/example/QuadraticTest.java
@@ -6,40 +6,65 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+import java.math.BigDecimal;
 
 /**
  * Unit test for simple App.
  */
 public class QuadraticTest
 {
+
+
     @Test
     public void simple()
     {
         QuadraticExpression simpleExpression = new QuadraticExpression(1, 1, -6);
-        assertEquals( "simpleExpression", 0.0 , simpleExpression.getValueFor(-3), 0.0);
-        assertEquals( "simpleExpression",  0.0 ,simpleExpression.getValueFor(2), 0.0);
+        assertEquals( "simpleExpression", new BigDecimal(0) , simpleExpression.getValueFor(-3));
+        assertEquals( "simpleExpression",  new BigDecimal(0) ,simpleExpression.getValueFor(2) );
     }
 
 
     @Test
     public void squareSolve() {
         QuadraticExpression squareExpression = new QuadraticExpression(1, 0, -9);
-        double[] solution = squareExpression.solve(0);
-        Assert.assertArrayEquals(new double[]{+3, -3}, solution, 0.0);
+        BigDecimal[] solution = squareExpression.solve(0);
+        BigDecimal[] expected = {new BigDecimal(3), new BigDecimal(-3)};
+        Assert.assertArrayEquals(expected, solution);
     }
 
     @Test
     public void simpleSolve() {
         QuadraticExpression simpleExpression = new QuadraticExpression(1, 1, -6);
-        double[] solution = simpleExpression.solve(0);
-        Assert.assertArrayEquals(new double[]{2, -3}, solution,0.0);
+        BigDecimal[] solution = simpleExpression.solve(0);
+        BigDecimal[] expected = {new BigDecimal(2), new BigDecimal(-3)};
+        Assert.assertArrayEquals(expected, solution);
+        Assert.assertEquals("first solution yields zero",
+                BigDecimal.ZERO, simpleExpression.getValueFor(solution[0]) );
+        Assert.assertEquals("second solution yields zero",
+                BigDecimal.ZERO, simpleExpression.getValueFor(solution[1]) );
     }
 
     @Test
     public void badSolve() {
+
         QuadraticExpression linearExpression = new QuadraticExpression(0, 4, -8);
-        double[] solution = linearExpression.solve(0);
-        assertTrue("invalid quadratic", java.lang.Double.isNaN(solution[0]));
-        assertTrue("invalid quadratic", java.lang.Double.isInfinite(solution[1]));
+
+        Assert.assertThrows(ArithmeticException.class,
+                () -> linearExpression.solve(0)
+             );
+    }
+
+    @Test
+    public void largeSolve() {
+        QuadraticExpression simpleExpression = new QuadraticExpression(1, -5e50, 6e100);
+        BigDecimal[] solution = simpleExpression.solve(0);
+
+        Assert.assertEquals("first solution yields zero",
+                0, BigDecimal.ZERO.compareTo(simpleExpression.getValueFor(solution[1])) );
+        Assert.assertEquals("second solution yields zero",
+                0, BigDecimal.ZERO.compareTo(simpleExpression.getValueFor(solution[1])) );
     }
 }
